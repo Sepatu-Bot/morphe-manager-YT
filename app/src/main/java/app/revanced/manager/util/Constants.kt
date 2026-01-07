41d1203f1183adb20c8d43e9b8937a759389240e
LisoUseInAIKyrios <118716522+lisouseinaikyrios@users.noreply.github.com>
2026-01-07 22:47:32 +0100

    fix: Some apk files are not selectable in expert mode

--- app/src/main/java/app/revanced/manager/util/Constants.kt
+++ app/src/main/java/app/revanced/manager/util/Constants.kt
@@ -6,19 +6,25 @@ const val tag = "Morphe Manager"
 const val APK_MIMETYPE = "application/vnd.android.package-archive"
 
 val APK_FILE_MIME_TYPES = arrayOf(
+    BIN_MIMETYPE,
     APK_MIMETYPE,
-    "application/zip",
-    "application/x-zip-compressed",
-    "application/x-apkm",
-    "application/x-apks",
-    "application/x-xapk",
-    "application/xapk",
-    "application/vnd.android.xapk",
-    "application/vnd.android.apkm",
-    "application/apkm",
-    "application/vnd.android.apks",
-    "application/apks",
-    BIN_MIMETYPE
+    // ApkMirror split files of "app-whatever123_apkmirror.com.apk" regularly Android to misclassify
+    // the file as an application or something incorrect. Renaming the file and
+    // removing "apkmirror.com" from the file name fixes the issue, but that's not something the
+    // end user will know or should have to do. Instead, show all files to ensure the user can
+    // always select no matter what file naming ApkMirror uses.
+    "application/*",
+//    "application/zip",
+//    "application/x-zip-compressed",
+//    "application/x-apkm",
+//    "application/x-apks",
+//    "application/x-xapk",
+//    "application/xapk",
+//    "application/vnd.android.xapk",
+//    "application/vnd.android.apkm",
+//    "application/apkm",
+//    "application/vnd.android.apks",
+//    "application/apks",
 )
 val APK_FILE_EXTENSIONS = setOf(
     "apk",
