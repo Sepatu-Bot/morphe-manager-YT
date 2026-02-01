d5ce30062370da6c73fff20675a126593d2a1b70
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2026-02-01 14:44:40 +0200

    chore: Remove duplicate

--- app/src/main/java/app/revanced/manager/ui/screen/settings/advanced/PatchOptionsDialogs.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/advanced/PatchOptionsDialogs.kt
@@ -135,7 +135,7 @@ fun ThemeColorDialog(
                         context,
                         desc,
                         DARK_THEME_COLOR_DESC,
-                        R.string.settings_advanced_patch_options_dark_theme_color_description
+                        R.string.settings_advanced_patch_options_theme_color_description
                     )
                     Text(
                         text = localizedDesc,
@@ -194,7 +194,7 @@ fun ThemeColorDialog(
                         context,
                         desc,
                         LIGHT_THEME_COLOR_DESC,
-                        R.string.settings_advanced_patch_options_light_theme_color_description
+                        R.string.settings_advanced_patch_options_theme_color_description
                     )
                     Text(
                         text = localizedDesc,
